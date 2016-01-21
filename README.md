518a46cc18038fa69fb3e1b439697dd2c8368079
Jean Parpaillon <jean.parpaillon@free.fr>
2016-01-21 11:29:26 +0100

    Use mustache for templating

---
+++ README.md
@@ -0,0 +1,8 @@
+generator-saloon
+================
+
+# Dependencies
+
+* yeoman-generator >= 0.18.0
+* generator-angular >= 0.15.1
+* mustache >= 2.2.1
--- app/index.js
+++ app/index.js
@@ -1,8 +1,21 @@
 'use strict';
 
 var generators = require('yeoman-generator');
+var mustache = require('mustache');
 
-var Saloon = generators.Base.extend();
+var Saloon = generators.Base.extend({
+    render: function (tmplFile, dest, ctx) {
+	var tmpl = this.fs.read(this.templatePath(tmplFile));
+	this.fs.write(this.destinationPath(dest), mustache.render(tmpl, ctx));
+    },
+
+    copy: function (orig, dest) {
+	if (typeof dest === "undefined") {
+	    dest = orig;
+	}
+	this.fs.copy(this.templatePath(orig), this.destinationPath(dest));
+    }
+});
 
 module.exports = Saloon.extend({
     initializing: function () {
@@ -13,13 +26,14 @@ module.exports = Saloon.extend({
     },
 
     writing: function () {
-	this.fs.copyTpl(
-	    this.templatePath('README.md.in'),
-	    this.destinationPath('README.md'),
-	    {
-		name: this.appname
-	    }
-	);
+	var ctx = {
+	    name: this.appname
+	};
+	this.render('README.md.in', 'README.md', ctx);
+	this.render('Makefile.in', 'Makefile', ctx);
+	this.render('start.sh.in', 'start.sh', ctx);
+	
+	this.copy('version.sh');
     },
 
     install: function () {
