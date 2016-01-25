75fc41988914c42b64035e65a1857b1b826c9da1
Jean Parpaillon <jean.parpaillon@free.fr>
2016-01-25 13:48:24 +0100

    testing: check all installed files

--- test/main.js
+++ test/main.js
@@ -15,7 +15,13 @@ describe('saloon', function () {
 		var appname = path.basename(process.cwd());
 		assert.file([
 			'.yo-rc.json', '.gitignore', 'README.md', 'Makefile', 'start.sh', 'version.sh',
-			'erlang.mk', 'include/' + appname + '_log.hrl'
+			'erlang.mk', 'include/' + appname + '_log.hrl',
+			'src/' + appname + '_app.erl', 'src/' + appname + '.app.src.in',
+			'src/' + appname + '.erl', 'src/' + appname + '_http.erl',
+			'src/' + appname + '_index.erl', 'src/' + appname + '_sup.erl',
+			'priv/www/bower.json', 'priv/www/index.html', 'priv/www/favicon.ico',
+			'priv/www/robots.txt', 'priv/www/styles/main.scss', 'priv/www/images/yeoman.png',
+			'priv/www/views/main.html', 'priv/www/views/view.html'
 		]);
 	});
 });
